b287ea4c581582062c0506c2f4dfb9432e21799b
Yusif Zourab <wordlearningpress@gmail.com>
2025-05-08 18:27:32 +0200

    TF-13

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\TrackController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('track')->group(function () {
+Route::prefix('tracks')->group(function () {
     Route::get('', [TrackController::class, 'index']);
     Route::post('', [TrackController::class, 'store']);
     Route::post('{track}', [TrackController::class, 'update']);
